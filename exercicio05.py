8520b223fc2b37ffaec6272b0bd36f8848aa622e
livia <annalivdeveloper@gmail.com>
2026-02-07 15:32:54 -0300

    otimizando o codigo

--- exercicio05.py
+++ exercicio05.py
@@ -1,46 +1,42 @@
-
 def ler_numero():
-  valor = input("Digite um número: (ou sair) ")
+    valor = input("Digite um número (ou sair): ")
 
-  if valor.lower == 'sair':
-   return None
-  
-  return int(valor)
- 
+    if valor.lower() == 'sair':
+        return None
+
+    if not valor.isnumeric():
+        print('Digite apenas números!')
+        return ler_numero()
+
+    return int(valor)
+
+
 while True:
- 
- numero1 = ler_numero()
 
- if numero1 is None:
-  break 
- 
- numero2 = ler_numero()
+    numero1 = ler_numero()
+    if numero1 is None:
+        break
 
- if numero2 is None:
-  break 
- 
- operacao = int(input("1 Soma | 2 Sub | 3 Mult | 4 Div: "))
+    numero2 = ler_numero()
+    if numero2 is None:
+        break
 
+    operacao = input("1 Soma | 2 Sub | 3 Mult | 4 Div: ")
 
+    if operacao == '1':
+        print(numero1 + numero2)
 
- soma = numero1 + numero2
- sub = numero1 - numero2
- mult = numero1 * numero2
- div = numero1 / numero2
+    elif operacao == '2':
+        print(numero1 - numero2)
 
+    elif operacao == '3':
+        print(numero1 * numero2)
 
- if operacao == 1:
-  print('O resultado da soma é {}.'.format(soma))
- elif operacao == 2:
-  print('O resultado da subtração é {}.'.format(sub))
- elif operacao == 3:
-  print('O resultado da multiplicação é {}.'.format(mult))
- elif operacao == 4:
-  if  numero2 == 0:
-   print('Não existe divisão por 0. ')
-  else:
-   print('O resultado da divisão é {}.'.format(div)) 
- else:
-  print('Operação inválida.')
-  
+    elif operacao == '4':
+        if numero2 == 0:
+            print("Não existe divisão por 0")
+        else:
+            print(numero1 / numero2)
 
+    else:
+        print("Operação inválida")
